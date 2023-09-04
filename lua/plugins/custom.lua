42fa4a96e88cf0b1f37e046b0f6e7d807acf3b6e
simoneeti <vintrob.simon@gmail.com>
2023-09-04 14:46:57 -0300

    custom

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,5 +1,9 @@
 return {
   {
+    'projekt0n/github-nvim-theme',
+    config = function() require('github-theme').setup {} end
+  },
+  {
     "phaazon/hop.nvim",
     config = function() require 'hop'.setup {} end,
     lazy = false
